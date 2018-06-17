6e16a5e5c4fa1d5c79cdc629877bbd9704070947
teddy <gedu07@gmail.com>
2018-06-17 01:03:38 -0300

    starting redux

---
+++ rick_and_morty/lib/redux/app_state.dart
@@ -0,0 +1,7 @@
+import 'package:rick_and_morty/model/character.dart';
+
+class AppState {
+
+  final List<Character> characters;
+
+}
---
+++ rick_and_morty/lib/resources/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+var primaryColor = Color(0xFF31303E);
+var secondaryColor = Color(0xFF85858C);
+
+var primaryAccent = Color(0xFF50E4C3);
+var secondaryAccent = Color(0xFF50A693);
