71259d6511f0365b21b0e4cf5110d1f9c6bb1e9d
teddy <gedu07@gmail.com>
2018-06-17 20:02:24 -0300

    created app state obj

--- rick_and_morty/lib/redux/app_state.dart
+++ rick_and_morty/lib/redux/app_state.dart
@@ -1,7 +1,24 @@
+import 'package:meta/meta.dart';
+import 'package:rick_and_morty/model/Episode.dart';
+import 'package:rick_and_morty/model/Region.dart';
 import 'package:rick_and_morty/model/character.dart';
 
 class AppState {
-
   final List<Character> characters;
+  final List<Region> regions;
+  final List<Episode> episodes;
 
+  const AppState({
+    @required this.characters,
+    @required this.regions,
+    @required this.episodes,
+  });
+
+  factory AppState.init() {
+   return AppState(
+       characters: List(),
+       regions: List(),
+       episodes: List(),
+   );
+  }
 }
